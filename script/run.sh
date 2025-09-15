336ea7e9f4dd90397af5c028cd54fd2f7856d200
janiejestemja <janiejestemja@gmail.com>
2025-09-15 21:47:20 +0200

    edit script

--- script/run.sh
+++ script/run.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
-# Python
-echo ">> Run Python"
-python py_src/main.py
 
-# Rust
-echo ">> Build and run Rust"
-cargo run
+# Python
+if [ "$1" == "py" ];then
+    echo ">> Run Python <<"
+    python py_src/main.py
+
 
 # Cpp
-echo ">> Build Cpp"
-g++ cp_src/main.cpp cp_src/lib.cpp -o cp_src/main
+elif [ "$1" == "cp" ];then
+    echo ">> Build Cpp <<"
+    g++ cp_src/main.cpp cp_src/lib.cpp -o cp_src/main
 
-echo "  >> Run Cpp"
-./cp_src/main
+    echo "  >> Run Cpp <<"
+    ./cp_src/main
+
+# Rust
+elif [ "$1" == "rs" ];then
+    echo ">> Build and run Rust <<"
+    cargo run
+
+elif [ "$1" == "--all" ];then
+    # Python
+    echo ">> Run Python <<"
+    python py_src/main.py
+
+    # Cpp
+    echo ">> Build Cpp <<"
+    g++ cp_src/main.cpp cp_src/lib.cpp -o cp_src/main
+
+    echo "  >> Run Cpp <<"
+    ./cp_src/main
+
+    # Rust
+    echo ">> Build and run Rust <<"
+    cargo run
+
+# Exit
+else
+    echo "CLA not supported"
+    exit 0 
+
+fi
