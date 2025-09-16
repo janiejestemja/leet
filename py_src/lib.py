92f78c926a9dcd100b01cbcfb907a6a722cd3175
janiejestemja <janiejestemja@gmail.com>
2025-09-16 20:04:40 +0200

    add binary search

--- py_src/lib.py
+++ py_src/lib.py
@@ -10,3 +10,19 @@ def qsort(arr):
     c = [x for x in arr if x == arr[pivot]]
     r = [x for x in arr if x > arr[pivot]]
     return qsort(l) + c + qsort(r)
+
+def search(arr, tar):
+    lower = 0
+    upper = int(len(arr) / 2)
+
+    while lower <= upper:
+        center = (lower + upper) // 2
+
+        if tar == arr[center]:
+            return center
+        elif tar > arr[center]:
+            lower += 1
+        else:
+            upper -= 1
+
+    return lower
--- py_src/main.py
+++ py_src/main.py
@@ -1,4 +1,4 @@
-from lib import add, qsort
+from lib import add, qsort, search
 
 
 def main():
@@ -7,7 +7,11 @@ def main():
 
     arr = [x % 7 for x in range(1, 50)]
     print(arr)
-    print(qsort(arr))
+    sorted_arr = qsort(arr)
+
+    index = search(arr, 0)
+    print(index)
+
 
 
 if __name__ == "__main__":
