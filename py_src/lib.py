156558ea5a422a6c10fa7986476caafc490e897b
janiejestemja <janiejestemja@gmail.com>
2025-09-16 03:11:33 +0200

    add qs

--- py_src/lib.py
+++ py_src/lib.py
@@ -1,2 +1,12 @@
 def add(left, right):
     return left + right
+
+
+def qsort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = len(arr) // 2
+    l = [x for x in arr if x < arr[pivot]]
+    c = [x for x in arr if x == arr[pivot]]
+    r = [x for x in arr if x > arr[pivot]]
+    return qsort(l) + c + qsort(r)
--- py_src/main.py
+++ py_src/main.py
@@ -1,9 +1,13 @@
-from lib import add
+from lib import add, qsort
 
 
 def main():
     print("works?")
     print(add(11, 13))
+
+    arr = [x % 7 for x in range(1, 50)]
+    print(arr)
+    print(qsort(arr))
 
 
 if __name__ == "__main__":
