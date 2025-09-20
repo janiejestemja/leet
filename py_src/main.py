3b90c2db34d775e538b151e49f4d56f91d414cf8
janiejestemja <janiejestemja@gmail.com>
2025-09-20 15:43:01 +0200

    add binary tree

--- py_src/main.py
+++ py_src/main.py
@@ -6,6 +6,41 @@ def main():
     print("works?")
     print(add(11, 13))
 
+    arr = [x for x in range(1, 8)]
+    tarr = Tree.from_arr(arr)
+    from_tarr = Tree.from_tree(tarr)
+    print(tarr)
+    print(from_tarr)
+
+
+class Tree:
+    def __init__(self, ele, left=None, right=None):
+        self.ele = ele
+        self.left = left
+        self.right = right
+
+    def __repr__(self):
+        return f"T({self.ele}, {self.left}, {self.right})"
+
+    @classmethod
+    def from_arr(cls, arr):
+        if len(arr) == 0:
+            return None
+        piv = len(arr) // 2
+        root = Tree(arr[piv])
+        root.left = cls.from_arr(arr[:piv])
+        root.right = cls.from_arr(arr[piv + 1:])
+
+        return root
+
+    @classmethod
+    def from_tree(cls, root):
+        if not root:
+            return []
+        left = cls.from_tree(root.left)
+        right = cls.from_tree(root.right)
+        return left + [root.ele] + right
+
 
 def circle_main():
     arr = [(-1)**(i % 3) * (x % 11) for i, x in enumerate(range(-22, 23))]
